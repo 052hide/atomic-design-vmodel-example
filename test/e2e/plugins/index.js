54dd628c2c89d057af73152b378456f89f8eaada
052hide <hidetoshi1990@gmail.com>
2020-10-15 01:03:08 +0900

    cypress lint fix

--- test/e2e/plugins/index.js
+++ test/e2e/plugins/index.js
@@ -1,4 +1,4 @@
-module.exports = (on, config) => {
+module.exports = (on, _config) => {
   on('before:browser:launch', (browser, launchOptions) => {
     if (browser.name === 'chrome' && browser.isHeadless) {
       launchOptions.args.push('--disable-gpu')
